b840c92ef99beac0ddbb5934b85bbba5e4c71079
Lincoln Gibson <lincolngibson7@gmail.com>
2024-01-19 16:35:25 -0300

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        hello world
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<div>Books</div>} />
+      <Route path="/categories" element={<div>Categories</div>} />
+      <Route path="*" element={<div>Not Found</div>} />
+    </Routes>
   )
 }
 
